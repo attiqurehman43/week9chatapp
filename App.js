dac6994d81857f007dc698601b15dc098132d68a
yasirnaeembjs <attiqurrehman43@gmail.com>
2023-01-31 15:42:17 +0500

    Persist Implemented

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
+
 import Routes from './src/Navigations/Routes';
-import store from './src/redux/store';
+import mystore from './src/redux/store';
 import {useEffect} from 'react';
 import {getItem} from './src/utils/utils';
 import {saveUserData} from './src/redux/reducers/auth';
 import persistStore from 'redux-persist/es/persistStore';
+import {PersistGate} from 'redux-persist/integration/react';
+
+const persistor = persistStore(mystore);
 
 const App = () => {
   useEffect(() => {
@@ -20,10 +23,10 @@ const App = () => {
   }, []);
 
   return (
-    <Provider store={store}>
-      {/* <PersistGate persistor={persistor}> */}
-      <Routes />
-      {/* </PersistGate> */}
+    <Provider store={mystore}>
+      <PersistGate persistor={persistor}>
+        <Routes />
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,19 @@
 import {configureStore} from '@reduxjs/toolkit';
-import reducer from './reducers';
+import rootReducer from './reducers/rootReducer';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import persistReducer from 'redux-persist/es/persistReducer';
 
-export default configureStore({
-  reducer,
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default mystore = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
